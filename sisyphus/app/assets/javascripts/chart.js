4c562de796aa8eab15be39b7060e8dacf2051df5
日出春来 <hinode@temona.co.jp>
2016-06-04 17:35:23 +0900

    [add]グラフ2つ追加。(まだデータ反映しない)

--- sisyphus/app/assets/javascripts/chart.js
+++ sisyphus/app/assets/javascripts/chart.js
@@ -25,7 +25,7 @@ FusionCharts.ready(function(){
         "showhovereffect": "1",
         "bgCOlor": "#ffffff",
         "borderAlpha": "0",
-        "cylFillColor": "eeeeee"
+        "cylFillColor": "#ffff00"
       },
       "value": "0"
     },
@@ -36,6 +36,87 @@ FusionCharts.ready(function(){
             FusionCharts("fuelMeter").feedData("&value=" + data.raw_data.match(/\d+/)[0]);
             $("#present-number").text(data.raw_data.match(/\d+/)[0] + "%")
           })
+        }, 1000);
+      }
+    }
+  }).render();
+});
+
+FusionCharts.ready(function(){
+  var fuelVolume = 110,
+  fuelWidget = new FusionCharts({
+    type: 'cylinder',
+    dataFormat: 'json',
+    id: 'fuelMeter2',
+    renderAt: 'chart-container2',
+    width: '200',
+    height: '250',
+    dataSource: {
+      "chart": {
+        "caption": "残業",
+        "subcaptionFontBold": "0",
+        "lowerLimit": "0",
+        "upperLimit": "100",
+        "lowerLimitDisplay": "Empty",
+        "upperLimitDisplay": "Full",
+        "numberSuffix": "%",
+        "showValue": "0",
+        "showhovereffect": "1",
+        "bgCOlor": "#ffffff",
+        "borderAlpha": "0",
+        "cylFillColor": "eeeeee"
+      },
+      "value": "0"
+    },
+    "events":{
+      "rendered": function(evtObj, argObj){
+        setInterval(function () {
+          socket.on("real_time_report2", function(data){
+            console.log(data)
+            FusionCharts("fuelMeter2").feedData("&value=" + data.raw_data.match(/\d+/)[0]);
+            $("#present-number").text(data.raw_data.match(/\d+/)[0] + "%")
+          })
+        }, 1000);
+      }
+    }
+  }).render();
+});
+
+FusionCharts.ready(function(){
+  var fuelVolume = 110,
+  fuelWidget = new FusionCharts({
+    type: 'cylinder',
+    dataFormat: 'json',
+    id: 'fuelMeter3',
+    renderAt: 'chart-container3',
+    width: '200',
+    height: '250',
+    dataSource: {
+      "chart": {
+        "caption": "残業",
+        "subcaptionFontBold": "0",
+        "lowerLimit": "0",
+        "upperLimit": "100",
+        "lowerLimitDisplay": "Empty",
+        "upperLimitDisplay": "Full",
+        "numberSuffix": "%",
+        "showValue": "0",
+        "showhovereffect": "1",
+        "bgCOlor": "#ffffff",
+        "borderAlpha": "0",
+        "cylFillColor": "F92500"
+      },
+      "value": "0"
+    },
+    "events":{
+      "rendered": function(evtObj, argObj){
+        setInterval(function () {
+          socket.on("real_time_report3", function(data){
+
+            FusionCharts("fuelMeter3").feedData("&value=" + data.raw_data.match(/\d+/)[0]);
+            $("#present-number").text(data.raw_data.match(/\d+/)[0] + "%")
+          })
+
         }, 1000);
       }
     }
